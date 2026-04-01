f41be6ef3c3f70978ab1c198d9e3fabba7e1eeb0
websturing <websturing.project@gmail.com>
2026-04-01 15:25:35 +0700

    Cleanup: menghapus file yang salah push

--- app/Features/Auth/routes.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Features\Auth\Controllers\AuthController;
-
-Route::get('/', [AuthController::class, 'index']);
-Route::post('/login', [AuthController::class, 'login']);
